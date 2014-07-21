fc2773dcd3a70a275311d2419cb4c2d0268876e4
Zeming YU <yuzeming@gmail.com>
2014-07-21 15:24:52 +0800

    添加首页

---
+++ PIPOSKY2/Models/DBModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PIPOSKY2.Models
+{
+    public class SKYDB : DbContext
+    {
+        public SKYDB()
+            : base("DbContext")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        [Key]
+        public int UserID { set; get; }
+
+        [Required]
+        public string UserName { set; get; }
+
+        [Required]
+        public string UserPwd { set; get; }
+    }
+}
