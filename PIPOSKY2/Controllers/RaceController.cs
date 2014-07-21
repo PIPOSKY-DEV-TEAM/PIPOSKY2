02e0c7df44aa96c1ee7ebc4a0ed3330db2818319
nezharen <nezharen@163.com>
2014-07-21 21:39:28 +0800

    RaceController

---
+++ PIPOSKY2/Controllers/RaceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PIPOSKY2.Controllers
+{
+    public class RaceController : Controller
+    {
+        //
+        // GET: /Race/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
