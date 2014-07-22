ad8c4b4acdcd1468427c47c34a4523ca82f742f3
Zeming YU <yuzeming@gmail.com>
2014-07-22 18:26:42 +0800

    添加用户注册功能（未完成）

---
+++ PIPOSKY2/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using PIPOSKY2.Models;
+
+namespace PIPOSKY2.Controllers
+{
+    public class UserController : Controller
+    {
+        private PIPOSKY2DbContext db = new PIPOSKY2DbContext();
+        [HttpPost]
+        public ActionResult Reg(RegFormModel info)
+        {
+            try
+            {
+                var tmp = new User();
+                tmp.UserName = info.UserName;
+                tmp.UserPwd = info.UserPwd;
+                tmp.UserEmail = info.UserEmail;
+                tmp.UserType = "";
+                db.Users.Add(tmp);
+                db.SaveChanges();
+                Session.Add("_massage", "注册成功");
+                Session.Add("_UserID",tmp.UserID);
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View(info);
+            }
+            
+        }
+
+        public ActionResult Reg()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ PIPOSKY2/Models/FormModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PIPOSKY2.Models
+{
+    public class LoginFormModel
+    {
+        [Required]
+        [Display(Name = "用户名")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "密码")]
+        public string UserPwd { get; set; }
+        [Required]
+        [Display(Name = "保持登录")]
+        public bool KeepLogin { get; set; }
+    }
+
+    public class RegFormModel
+    {
+        [Required]
+        [Display(Name = "用户名")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "密码")]
+        public string UserPwd { get; set; }
+        [Required]
+        [Display(Name = "确认密码")]
+        public string UserPwd2 { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string UserEmail { get; set; }
+    }
+}
