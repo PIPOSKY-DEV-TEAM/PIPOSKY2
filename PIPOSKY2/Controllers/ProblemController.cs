4fd5e47d0b9c32aab9589a14acecaa73a207f747
charlotte <lingboxunmeng@163.com>
2014-07-23 14:14:10 +0800

    ProblemController建立，layout链接修改

---
+++ PIPOSKY2/Controllers/ProblemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PIPOSKY2.Controllers
+{
+    public class ProblemController : Controller
+    {
+        //
+        // GET: /Problem/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
